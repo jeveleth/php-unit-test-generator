b98cf34fb44506d7565b3511656f98bbe55ca2c2
Josh Eveleth <joshua.eveleth@gmail.com>
2016-05-10 23:55:22 -0700

    Writes it real good

--- cli-apps/src/TestGenerator.php
+++ cli-apps/src/TestGenerator.php
@@ -25,12 +25,6 @@ class TestGenerator extends Command
                 'testFilePath',
                 InputArgument::REQUIRED,
                 'What class are you testing?'
-             )
-             ->addOption(
-                'yell',
-                null,
-                InputOption::VALUE_NONE,
-                'If set, the task will yell in uppercase letters'
         );
     }
 
@@ -45,7 +39,7 @@ class TestGenerator extends Command
         $filePath = $input->getArgument('testFilePath');
         $functions = $this->parseFile($filePath);
         $finalTestFunctions = $this->returnTestFunctions($functions);
-        $this->writeFunctionsToFile($finalTestFunctions);
+        $this->writeFunctionsToFile($finalTestFunctions, $filePath);
     }
 
     /**
@@ -75,25 +69,45 @@ class TestGenerator extends Command
     protected function returnTestFunctions($functions)
     {
         $newFunctions = array();
+
         foreach ($functions as $key => $value) {
             $newFunctions[] = stristr($value, 'function');
         }
         $finalFunctions = array();
+
         foreach ($newFunctions as $key => $value) {
             $finalFunctions[] = preg_replace('/function /', '', $value);
         }
 
         $finalTestFunctions = array();
+
         foreach ($finalFunctions as $key => $value) {
             $rawFunction = preg_replace('/\(+.*\)+/i', '', $value);
             $upcaseFunction = ucfirst($rawFunction);
-            $finalTestFunctions[]= "test$upcaseFunction()\n{\n}";
+            $finalTestFunctions[]= "\n\tpublic function test$upcaseFunction()\n\t{\n\n\t}\n";
         }
         return $finalTestFunctions;
     }
 
-    protected function writeFunctionsToFile($finalTestFunctions)
+    /**
+     * [writeFunctionsToFile description]
+     * @param  [type] $finalTestFunctions [description]
+     * @param  [type] $filePath           [description]
+     * @return [type]                     [description]
+     */
+    protected function writeFunctionsToFile($finalTestFunctions, $filePath)
     {
-        # code...
+        $filePath = rtrim($filePath, '.php');
+        $pathMatches = preg_match('/([a-z0-9]+\/)+/i', $filePath, $match);
+        $class =  ltrim($filePath, $match[0]);
+        $filePath = "{$filePath}Test.php";
+
+        $handle = fopen($filePath, 'w');
+        fwrite($handle, "<?php\n\nclass {$class}Test extends \PHPUnit_Framework_TestCase\n{\n");
+        foreach ($finalTestFunctions as $key => $value) {
+            fwrite($handle, $value);
+        }
+        fwrite($handle, "\n}");
+        fclose($handle);
     }
 }
